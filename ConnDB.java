eed960cf50d4af99a1e44604d2b6837d944f6ca1
xwq <cruiserdou@gmail.com>
2013-03-07 00:52:32 +0800

    Create ConnDB.java
    Java Connect DB2 Database sample.

---
+++ ConnDB.java
@@ -0,0 +1,28 @@
+package xwq;
+import java.sql.*;
+
+public class ConnDB {
+
+  /**
+	 * @param args
+	 */
+	
+	public static void main(String[] args) throws SQLException, Exception{
+		try{
+			Class.forName("com.ibm.db2.jcc.DB2Driver").newInstance();
+			String url = "jdbc:db2://192.168.1.106:50000/sample";
+			String user = "db2inst1";
+			String password = "Db2";
+			Connection conn = DriverManager.getConnection(url, user, password);
+			System.out.println("Connection Done!");
+			Statement stmt = conn.createStatement();
+			String sql = "select * from act";
+			ResultSet rs = stmt.executeQuery(sql);
+			while (rs.next()){
+				System.out.println(rs.getString(1) + " " + rs.getString(2));
+			}
+		}catch(SQLException e){
+			System.out.println(e);
+		}
+	}
+}
